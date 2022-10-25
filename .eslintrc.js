0aeaea8d910156418e53b6ee24f85f83273a960d
anhquanduong2007 <quanduongforworks2007@gmail.com>
2022-10-25 20:23:11 +0700

    update rule eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -17,6 +17,17 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    "prettier/prettier": ["error", {
+      "endOfLine": false,
+      "printWidth": 50,
+      "trailingComma": "es5",
+      "semi": false,
+      "doubleQuote": true,
+      "jsxSingleQuote": true,
+      "singleQuote": false,
+      "useTabs": false,
+      "tabWidth": 4
+    }],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
